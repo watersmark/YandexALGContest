4230ff1e020989361c40d128b05f81ca66e08e1b
watersmark <watersmark@mail.ru>
2020-11-05 17:59:05 +0300

    Realise FinalSp1PrB

--- FinalProjectSp1/TempB/src/Solution.java
+++ FinalProjectSp1/TempB/src/Solution.java
@@ -1,38 +1,3 @@
-import javax.swing.*;
-import javax.swing.table.TableRowSorter;
-
-class Node<V> {
-    private V value;
-
-    private Node<V> next;
-
-    public Node(V value, Node<V> next) {
-        this.value = value;
-        this.next = next;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public Node<V> getNext() {
-        return next;
-    }
-
-    public void setNext(Node<V> next) {
-        this.next = next;
-    }
-
-    public boolean hasNext() {
-        return next != null;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-}
-
-
 // Класс для решения задачи
 public class Solution {
 
@@ -67,14 +32,5 @@ public class Solution {
             }
 
         }
-    }
-}
-
-
-class Start{
-    public static void main(String[] args) {
-
-
-
     }
 }
