fc65c93f910305bbea217a5bc27dc2dafd1dd7ef
watersmark <watersmark@mail.ru>
2020-11-09 00:10:27 +0300

    add Sp2FinalPrB

--- Sp2/FinalProblemSp2/B/src/ProblemB.java
+++ Sp2/FinalProblemSp2/B/src/ProblemB.java
@@ -13,29 +13,6 @@ public class ProblemB {
     // Рекурсивная функция для поиска элемента
     public static void recLog(int indexNow, int LeftEnd, int RightEnd, boolean isEnd) {
 
-
-        // Разберём случай рекурсивного возврата
-        // и случай для оптимизации быстрого возврата(частный случай)
-        if (Integer.parseInt(massElems[0]) >= searchElem) {
-
-            // Случай первого индекса
-            if (Integer.parseInt(massElems[0]) == searchElem) {
-                System.out.println("0");
-                return;
-            }
-
-            // Случай последнего индекса, когда мы ничего не нашли
-            if (Integer.parseInt(massElems[massElems.length - 1]) != searchElem) {
-                System.out.println("-2");
-                return;
-            }
-
-            // Случай когда нашли в последнем индексе
-            System.out.println(massElems.length - 1);
-            return;
-        }
-
-
         // Теперь перейдём к случаю возврата из рекурсии(Общий случай)
         // Если ничего не нашли вернём -1
         // Так же проверим не попали ли мы на нужный нам элемент
@@ -59,21 +36,22 @@ public class ProblemB {
         // Двигаемся в левую сторону
         if (Integer.parseInt(massElems[indexNow]) > searchElem) {
 
-            if((int) Math.round(((double) indexNow - LeftEnd) / 2 ) == 1
-                    || (int) Math.round(((double) indexNow - LeftEnd) / 2 ) == 0){
+            if ((int) Math.round(((double) indexNow - LeftEnd) / 2) == 1
+                    || (int) Math.round(((double) indexNow - LeftEnd) / 2) == 0) {
+
                 isEnd = true;
             }
-            recLog(indexNow - (int) Math.round(((double) indexNow - LeftEnd) / 2 ), LeftEnd, indexNow, isEnd);
+            recLog(indexNow - (int) Math.round(((double) indexNow - LeftEnd) / 2), LeftEnd, indexNow, isEnd);
 
         } else { // Двигаемся в правую сторону
 
-            if((int) Math.round(((double) RightEnd - indexNow) / 2) == 1
-                    ||(int) Math.round(((double) RightEnd - indexNow) / 2) == 0 )
-            {
+            if ((int) Math.round(((double) RightEnd - indexNow) / 2) == 1
+                    || (int) Math.round(((double) RightEnd - indexNow) / 2) == 0) {
+
                 isEnd = true;
             }
 
-            recLog(RightEnd - (int) Math.round(((double) RightEnd - indexNow) / 2) , indexNow, RightEnd, isEnd);
+            recLog(RightEnd - (int) Math.round(((double) RightEnd - indexNow) / 2), indexNow, RightEnd, isEnd);
         }
 
 
@@ -94,7 +72,28 @@ public class ProblemB {
         massElems = reader.readLine().split(" ");
 
         // Начало метода recLog
-        recLog(Math.round((massElems.length - 1) / 2f), 0, massElems.length - 1, false);
+        //recLog(Math.round((massElems.length - 1) / 2f), 0, massElems.length - 1, false);
+
+        // Рассмотрим три возможныъ случая нахождения числа
+        // с помощью рекурсии
+
+        // Классический случай
+        if (searchElem >= Integer.parseInt(massElems[0])) {
+
+            recLog(Math.round((massElems.length - 1) / 2f), 0, massElems.length - 1, false);
+
+        } // Случай когда элемент может находиться в кольцевой части
+        else if (Integer.parseInt(massElems[0]) > searchElem
+                && Integer.parseInt(massElems[massElems.length - 1]) < Integer.parseInt(massElems[0])
+                && Integer.parseInt(massElems[massElems.length - 1]) >= searchElem) {
+
+            recLog(massElems.length - 1, 0, massElems.length - 1, false);
+
+        } // Случай постоянного возрастания, элемента быть не может
+        else if (Integer.parseInt(massElems[0]) < Integer.parseInt(massElems[massElems.length - 1])) {
+            System.out.println("-1");
+        }
+
 
     }
 }
